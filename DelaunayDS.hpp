cf7bf78162dfdd44e27547a5ebbfb0e3f5c6d1c4
YoungjoMin <tuna10516@kaist.ac.kr>
2021-07-16 15:14:39 +0900

    add more functions, now node saves triangle points

--- DelaunayDS.hpp
+++ DelaunayDS.hpp
@@ -4,18 +4,19 @@ struct Triangle;
 struct Node;
 struct PointLocation;
 
-struct Triangle {
-    int p1, p2, p3; //ccw order
+struct Triangle { //for legalize, save adj triangle info
     Node * node;
-    Triangle * adj12,* adj23,* adj31;
-    Triangle(int p1, int p2, int p3): p1(p1), p2(p2), p3(p3) {}
+    Triangle * adj[3];
+    Triangle() {}
 };
 struct Node {
+    int p1, p2, p3; //ccw order
+    //edge order = 12, 23, 31
     Node *c1,*c2,*c3;//child
     Triangle * t;// is it's not leaf, then t = NULL
 
     //to find suitable child
-    Vector pt;
+    int pt;// point that divide this node
     enum Divide {
         NotDivided,
         BySinglePt,
@@ -24,11 +25,18 @@ struct Node {
         ByPtOnEdge31
     } divideBy;
 
-    Node();
+    bool visited;//for gathering all Triangles at the last
+
+    Node(int p1, int p2, int p3);
     bool isLeaf() const;
+    Node * findChild(int idx);//assert this node have child 
+
 };
 
 struct PointLocation {
     Node * root;
     PointLocation(const Vector& bl, const Vector& tr); //Point location structure that all points are bounded by bl, tr
+    void insert(int idx);//insert idx's point
+    void Legalize(Node * node, int idx);
+    void gatherAllLeafs(std::vector<Node *>& buffer);
 };
